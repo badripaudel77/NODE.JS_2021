a2855e8cbc288fe2b206748c9b4cf89fd16155cc
badripaudel77 <badripaudel77@gmail.com>
2021-05-06 23:11:15 +0545

    static files serverd

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 //ref : https://www.digitalocean.com/community/tutorials/how-to-use-ejs-to-template-your-node-application
 app.set('view engine', 'ejs');
 
+//middleware for css [static files]
+app.use(express.static('public'))
 const PORT = 3001 || process.env.PORT
 app.listen(PORT, (req, res) => {
     console.log("Server started running")
@@ -14,13 +16,13 @@ app.get('/', (req,res) => {
     //send backto index page
     //res.sendFile('./views/index.html', { root : __dirname})
     //views is default that express is gonna look, if not set like app.set('views', 'foldername')
-    res.render('index')
+    res.render('index', {indexTitle : 'My Node.js 2021 Lockdown - Nepal'})
 })
 
 app.get('/about', (req,res) => {
     //send backto index page
     //res.sendFile('./views/about.html', { root : __dirname})
-    res.render('about')
+    res.render('about', {aboutTitle : 'About Page'}) //pass the data [title]
 })
 
 app.use((req, res) => {
